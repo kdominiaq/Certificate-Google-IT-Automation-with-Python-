00adb825835800c4a5013157926c69b8d6ea60bc
Kacper Dominiak <69914937+kdominiaq@users.noreply.github.com>
2021-11-19 11:02:29 +0100

    Add statments

---
+++ Automating Real-World Tasks with Python/Week 4/changeImage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#Import necessary modules
+import os, sys
+from PIL import Image
+
+#Declare variables to be used
+user = os.getenv('USER') # To get the username from environment variable
+image_directory = '/home/{}/supplier-data/images/'.format(user)
+
+for image_name in os.listdir(image_directory):
+    if not image_name.startswith('.') and 'tiff' in image_name:
+        image_path = image_directory + image_name
+        path = os.path.splitext(image_path)[0]
+        im = Image.open(image_path)
+        new_path = '{}.jpeg'.format(path)
+        im.convert('RGB').resize((600, 400)).save(new_path, "JPEG")
+        
+# Statment:
+# Size: Change image resolution from 3000x2000 to 600x400 pixel
+# Format: Change image format from .TIFF to .JPEG
